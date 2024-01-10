6741ef850b6a2e34a93d0300108261b234d13072
Mario Parra <marioparra375@gmail.com>
2024-01-10 13:32:01 -0800

    add login

--- app/src/main/java/parra/mario/geofencing/LoginActivity.kt
+++ app/src/main/java/parra/mario/geofencing/LoginActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
     lateinit var btn_registrarse: Button
     lateinit var btn_ingresar: Button
     lateinit var tv_olvideContra: TextView
-
+    lateinit var campo_correo: EditText
+    lateinit var campo_contra: EditText
+    private lateinit var auth: FirebaseAuth;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -18,6 +25,10 @@ class LoginActivity : AppCompatActivity() {
         btn_registrarse = findViewById(R.id.btn_registrarse);
         btn_ingresar = findViewById(R.id.btn_ingresar);
         tv_olvideContra = findViewById(R.id.tv_olvidasteContra);
+        campo_correo = findViewById(R.id.et_correo);
+        campo_contra = findViewById(R.id.et_contra)
+
+        auth = Firebase.auth
 
         btn_registrarse.setOnClickListener{
             var intent: Intent = Intent(this, RegistroActivity::class.java);
@@ -27,6 +38,32 @@ class LoginActivity : AppCompatActivity() {
         tv_olvideContra.setOnClickListener {
             var intent: Intent = Intent(this, ContrasenaActivity::class.java)
             startActivity(intent)
+        }
+
+        btn_ingresar.setOnClickListener {
+            var correo = campo_correo.text.toString();
+            var contra = campo_contra.text.toString();
+
+            if(!correo.isNullOrEmpty() && !contra.isNullOrEmpty()){
+                auth.signInWithEmailAndPassword(correo, contra)
+                    .addOnCompleteListener{task ->
+                        if(task.isSuccessful){
+
+                            val usuario = auth.currentUser;
+
+                            var intent: Intent = Intent(this, InicioActivity::class.java);
+                            startActivity(intent);
+
+                        }else{
+                            Toast.makeText(this, "Error al ingresar", Toast.LENGTH_LONG).show();
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "Ingresar datos", Toast.LENGTH_LONG).show();
+            }
+
+
+
         }
 
     }
