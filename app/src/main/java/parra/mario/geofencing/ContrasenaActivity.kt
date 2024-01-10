92d0c396d9fc5c2e1ae0a774ce1d2d520189528b
Mario Parra <marioparra375@gmail.com>
2024-01-10 12:58:53 -0800

    add registro

--- app/src/main/java/parra/mario/geofencing/ContrasenaActivity.kt
+++ app/src/main/java/parra/mario/geofencing/ContrasenaActivity.kt
@@ -2,10 +2,44 @@ package parra.mario.geofencing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class ContrasenaActivity : AppCompatActivity() {
+    lateinit var btn_restablecer: Button
+    lateinit var et_correo_cont: EditText
+    private lateinit var auth: FirebaseAuth;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contrasena)
+
+        btn_restablecer = findViewById(R.id.btn_restablecer);
+        et_correo_cont = findViewById(R.id.et_correo_cont);
+
+        auth = Firebase.auth
+
+        btn_restablecer.setOnClickListener{
+            var correo = et_correo_cont.text.toString();
+
+            if(!correo.isNullOrEmpty()){
+
+                auth.sendPasswordResetEmail(correo)
+                    .addOnCompleteListener{ task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "se envió un correo a $correo", Toast.LENGTH_SHORT).show()
+
+                        }else{
+                            Toast.makeText(this, "error al enviar correo", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+            }else{
+                Toast.makeText(this, "ingresar un correo", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/parra/mario/geofencing/RegistroActivity.kt
+++ app/src/main/java/parra/mario/geofencing/RegistroActivity.kt
@@ -2,10 +2,66 @@ package parra.mario.geofencing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
 
 class RegistroActivity : AppCompatActivity() {
+    lateinit var btnRegistrar: Button
+    lateinit var campo_correo: EditText
+    lateinit var campo_contra: EditText
+    lateinit var campo_contra2: EditText
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+        btnRegistrar = findViewById(R.id.btn_registrar);
+        campo_correo = findViewById(R.id.et_correo_reg);
+        campo_contra = findViewById(R.id.et_contra_reg);
+        campo_contra2 = findViewById(R.id.et_contra2_reg);
+
+        auth = Firebase.auth
+
+        btnRegistrar.setOnClickListener{
+            registar();
+        }
+    }
+
+    private fun registar(){
+        var correo = campo_correo.text.toString();
+        var contra1 = campo_contra.text.toString();
+        var contra2 = campo_contra2.text.toString();
+
+        if(!correo.isNullOrBlank() && !contra1.isNullOrEmpty() && !contra2.isNullOrEmpty()){
+
+            if(contra1 == contra2){
+
+                registrarFirebase(correo, contra1);
+
+            }else{
+                Toast.makeText(this, "la contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            }
+
+        }else{
+            Toast.makeText(this, "Ingresar datos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private fun registrarFirebase(correo: String, contra: String){
+        auth.createUserWithEmailAndPassword(correo, contra)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    val usuario = auth.currentUser
+                    Toast.makeText(this, "${usuario?.email} se registró correctamente", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this, "No se pudo ingresar", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
