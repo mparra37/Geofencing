eca3952c00a5b2b21e72b466db8217138352531e
Mario Parra <marioparra375@gmail.com>
2024-03-12 10:17:05 -0700

    menu design

--- app/src/main/java/parra/mario/geofencing/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/parra/mario/geofencing/ui/gallery/GalleryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "Lista de Ubicaciones"
     }
     val text: LiveData<String> = _text
 }
